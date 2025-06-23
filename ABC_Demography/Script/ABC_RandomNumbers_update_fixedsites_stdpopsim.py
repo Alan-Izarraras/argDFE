d43d686df796b716c9dfa02fab2ca93a22ef3a93
Alan-Izarraras <62118545+Alan-Izarraras@users.noreply.github.com>
2025-06-23 17:14:45 -0600

    WIP restructuring main and python script

--- ABC_Demography/Script/ABC_RandomNumbers_update_fixedsites_stdpopsim.py
+++ ABC_Demography/Script/ABC_RandomNumbers_update_fixedsites_stdpopsim.py
@@ -1,22 +1,16 @@
-##Es llamado por Run_ABC.sh y genera corridas aleaotrias de una distribución de demografias.
-##Code modified for stdpopsim runs. mutation rate + total num of sites is adjusted
-#Further modifyied PrsentSize range from 100k to 40k max. Also l to 26k sites instead of 32k sites
+
+##This script generates random draws for demographic parameter distributions and writes parameter files to run
+##prefersimulations under that particular demographic model. The output is trees under the simulation. 
 
 import sys , random , math
 
-###Corre con 2 valores: Number + RandomSeed (quedan igual)
-
-###Creo que puedo comentar eso
+#Why do I need 2 of the same argument??
+#Arguments from broader environment 
 print (sys.argv[1])
 Number = int(sys.argv[1])
-RandomSeed = int(sys.argv[2])
-#Fraction = float(sys.argv[3])
 
-### Poner la semilla aleatoria. Por ahora funciona siempre y cuando definas la semilla, luego la pongo automatica
-#Number = 11
-#RandomSeed = 11
-
-random.seed( RandomSeed )
+#uses random to generate a seed based on the slurm_id_run 
+random.seed( Number )
 
 #Parametros de msel
 
@@ -28,13 +22,6 @@ RandFile = open (RandomNumbersFile, 'w')
 ParsFile = open (ParamsFile, 'a')
 
 #### Prior distributions
-#### N_pasado (101 - 5,000), N_presente (1,000 - 1,000,000), t (0 - 1,500)
-### Update Run #4 N pasado se queda igual N presente (1,000 - 500,000), t(0 - 20000).
-
-###Past from 1 to 15,000
-###Present from 1,000 to 40,000
-###Time 0 to 20k
-
 NeInitial = random.random() * 1.602  # 5 - 2.999 = 2.001
 print("Ne Initial " + str(NeInitial))
 NeInitial = int(10**(NeInitial + 2.999) + 1 + 0.5)
@@ -43,24 +30,25 @@ NePast = random.random() * 14899
 NePast = int(NePast + 101 + 0.5)
 Time = random.random() * 13000
 Time = int(Time + 0.5)
+
 ###Parametros de msel
-#Roh es para cosas de recombinación
 TimeFraction = float( int(Time) / ( 4.0 * NeInitial))
 PopFraction = float ( int (NePast) / ( 1.0 *  NeInitial))
-Rho=1
-#Parsea ParamFiles con datos de arriba.
+Rho=1 #recombination, but prefersim assumes no recombination so =1 
+#Writes current drawn demography to file
 String = str(NeInitial) + "\t" + str(NePast) + "\t" + str (Time) + "\n"
 ParsFile.write(String)
 
 ###msel TimeFraction and PopFraction parameters for tree generation, this creates the file "LeftRandomNumbers"
+#Why do I make a for loop here..? 
 
 for x in range(1):
    String = str(TimeFraction) + "\t" + str(PopFraction) + "\n"
    RandFile.write(String)
 RandFile.close()
 
-#Yeah so right here I should... use coding mutation rate. Cuz Im fairly sure I used neutral dfe sites.
-#Confirm that that is the mutation rate. yh... fairly sure that is it. 
+#Parameters subject to change depending on source data
+# l is the number of sites and dictates how much a single demography run is split. Paralel running. 
 
 u = 2.35e-08 ##stdpopsim mut rate based on demography = 2.35e-8
 l = 962697 #Total number of synonimous sites found in H-like chr1 #Está mal. deberia ser 962697
@@ -132,8 +120,6 @@ String = str(int( 2.0 * NePast +0.5)) + " " + str(int((NePast * 16)+0.5)) + "\n"
 String = String + str(int(2.0 * NeInitial+0.5)) + " " + str(int((Time )+0.5)) + "\n"
 
 #Tengo que agregar TimeFraction y PopFraction
-
-
 DemHistFile.write(String)
 
 #Cerramos los archivos que escribimos.
@@ -142,3 +128,5 @@ DemHistFile.close()
 ABCParsFileB.close()
 RandFile.close()
 ParsFile.close()
+
+#WIP pending tests 
