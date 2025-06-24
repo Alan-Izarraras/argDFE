fd8eaee9afc682f508da3fb883a0f129c643bffb
Alan-Izarraras <62118545+Alan-Izarraras@users.noreply.github.com>
2025-06-24 16:30:49 -0600

    slurm migration wip

--- ABC_Demography/Script/ABC_RandomNumbers_update_fixedsites_stdpopsim.py
+++ ABC_Demography/Script/ABC_RandomNumbers_update_fixedsites_stdpopsim.py
@@ -1,35 +1,36 @@
-
 ##This script generates random draws for demographic parameter distributions and writes parameter files to run
 ##prefersimulations under that particular demographic model. The output is trees under the simulation. 
+##Add a message that says which dataset this is running for (stdpopsim, relate tree, 1k genomes, etc)
 
 import sys , random , math
 
 #Why do I need 2 of the same argument??
 #Arguments from broader environment 
-print (sys.argv[1])
+print(f"Initial seed value is: {sys.argv[1]} \n")
 Number = int(sys.argv[1])
 
 #uses random to generate a seed based on the slurm_id_run 
 random.seed( Number )
 
 #Parametros de msel
-
 ###Requiero ultimas 2 columnas de "LeftRandomNumbers" $number = SGE_TASK_ID --> paralelizar
-RandomNumbersFile = "../Results/ABCAnalysisPopExpansion/DemographyScales" + str(Number) + ".txt"
-ParamsFile = "../Results/ABCAnalysisPopExpansion/DemographyData" + str(Number) + ".txt"
+RandomNumbersFile = "../Data/ABCAnalysisPopExpansion/DemographyScales" + str(Number) + ".txt"
+ParamsFile = "../Data/ABCAnalysisPopExpansion/DemographyData" + str(Number) + ".txt"
 
 RandFile = open (RandomNumbersFile, 'w')
 ParsFile = open (ParamsFile, 'a')
 
 #### Prior distributions
 NeInitial = random.random() * 1.602  # 5 - 2.999 = 2.001
-print("Ne Initial " + str(NeInitial))
+#print("Ne Initial " + str(NeInitial))
 NeInitial = int(10**(NeInitial + 2.999) + 1 + 0.5)
-print("Ne Initial " + str(NeInitial))
+print(f"Ne Initial is :{NeInitial}")
 NePast = random.random() * 14899
 NePast = int(NePast + 101 + 0.5)
+print(f"Ne Past is :{NePast}")
 Time = random.random() * 13000
 Time = int(Time + 0.5)
+print(f"Time in generations is :{Time}\n")
 
 ###Parametros de msel
 TimeFraction = float( int(Time) / ( 4.0 * NeInitial))
@@ -51,23 +52,27 @@ RandFile.close()
 # l is the number of sites and dictates how much a single demography run is split. Paralel running. 
 
 u = 2.35e-08 ##stdpopsim mut rate based on demography = 2.35e-8
-l = 962697 #Total number of synonimous sites found in H-like chr1 #Está mal. deberia ser 962697
-l = l / 40  #this should make 3x faster without compromising much --> previous was 300 arund 26k sites per run
+print(f"Using mutation rate {u}")
+l = 962697 #Total number of synonimous sites found in H-like chr1
+print(f"Using total number of sites {l}")
+run_number=40
+l = l / run_number  #this should make 3x faster without compromising much --> previous was 300 arund 26k sites per run
+print(f"Divided into {run_number} independent runs \n")
 theta = (NePast * 4 * u * l)
 theta = str(theta)
 
 #Number identifica la corrida. #Cambia el nombre
-ParamsFile =  "../Results/ABCAnalysisPopExpansion/ParameterFile_" + str(Number) + ".txt"
-ParamsFileB = "../Results/ABCAnalysisPopExpansion/ParameterFile_" + str(Number) + "_B.txt"
+ParamsFile =  "../Data/ABCAnalysisPopExpansion/ParameterFile_" + str(Number) + ".txt"
+ParamsFileB = "../Data/ABCAnalysisPopExpansion/ParameterFile_" + str(Number) + "_B.txt"
 
 ABCParsFile = open (ParamsFile, 'w')
 ABCParsFileB = open (ParamsFileB, 'w')
 
 #Archvis de salida Demografia, Output
-DemHist = "../Results/ABCAnalysisPopExpansion/Output/DemographicHistory" + str(Number) + ".txt"
-OutDir = "../Results/ABCAnalysisPopExpansion/Output/Output"
-AllelesFile = "../Results/ABCAnalysisPopExpansion/Output/Alleles_" + str(Number) + ".txt"
-TrajFile = "../Results/ABCAnalysisPopExpansion/Output/Traj_" + str(Number) + ".txt"
+DemHist = "../Data/ABCAnalysisPopExpansion/Output/DemographicHistory" + str(Number) + ".txt"
+OutDir = "../Data/ABCAnalysisPopExpansion/Output/Output"
+AllelesFile = "../Data/ABCAnalysisPopExpansion/Output/Alleles_" + str(Number) + ".txt"
+TrajFile = "../Data/ABCAnalysisPopExpansion/Output/Traj_" + str(Number) + ".txt"
 
 #Cambiar directorios (Out)
 DemHistFile = open (DemHist,'w')
