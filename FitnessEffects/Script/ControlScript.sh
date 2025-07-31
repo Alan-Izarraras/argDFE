477e1a3b26f780b2531129c453ad4ef88c574c33
Alan Raymundo Izarraras Gómez <aizarraraz@login01.cluster>
2025-07-30 18:43:05 -0600

    Simulating selection values given a demography is now working

--- FitnessEffects/Script/ControlScript.sh
+++ FitnessEffects/Script/ControlScript.sh
@@ -5,6 +5,6 @@ for x in $(seq 1000 1000 28000)
 do
     qsub1=$(expr ${x} + 1)
     qsub2=$(expr ${x} + ${num_corridas})
-    qsub -t $qsub1-$qsub2 Posterior_SelectionRuns_reducido_optimizado_update_parallel_yri.sge
+    sbatch -a $qsub1-$qsub2 Posterior_SelectionRuns_reducido_optimizado_update_parallel_yri.slurm
     echo se esta corriendo $qsub1 a $qsub2
 done
