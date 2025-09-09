b4ad895165e0ab090af78cd6fb76d8e4c69f8085
Alan Raymundo Izarraras Gómez <aizarraraz@login01.cluster>
2025-09-09 15:38:33 -0600

    Fixed bugs on parameters being passed

--- ABC_Demography/Script/ABC_StableParameters.py
+++ ABC_Demography/Script/ABC_StableParameters.py
@@ -1,22 +1,17 @@
-#ABC_ReferenceSimulationRun.py 
 
-###This script is meant to be a way to simulate the "observed" data that will be used to compare against the 
-# regular ABC generations. It should only be ran once. Modified from the regular script that sampled from distributions.
-#oh wait. The difference will be that I wont be able to paralelize... and it will be sent as a single sim. 
-#So it might not be the most comvinient setup? 
-#no wait, in principle its the same thing, I just run it with what ever number of seeds I need. 
+###This script simulates the "observed" data that will be used to compute distance from the monte carlo (ABC) generations
+#The only difference is that priors are fixed or stable rather than being sampled from a distribution.
+#The user is still able to choose the stable parameters.  
 #Just the final step that takes the trees and pulls them together and created the matrix is different. 
-#Ok first lets test how slow this is, with the full variation. If its incrediblu slow then lets paralelize. 
 
-import sys , random , math
+import random
+import argparse
+import os
+import sys
+import math
 
-#Why do I need 2 of the same argument??
-#Arguments from broader environment
-print(f"Initial seed value is: {sys.argv[1]} \n")
-Number = int(sys.argv[1])
-
-#uses random to generate a seed based on the slurm_id_run
-random.seed( Number )
+seed = os.environ.get('SLURM_ARRAY_TASK_ID')
+Number=seed
 
 #Parameters de msel
 ###Requiero ultimas 2 columnas de "LeftRandomNumbers" $number = SGE_TASK_ID --> paralelizar
@@ -26,29 +21,52 @@ ParamsFile = "../Data/Parameters/DemographyData" + str(Number) + ".txt"
 RandFile = open (RandomNumbersFile, 'w')
 ParsFile = open (ParamsFile, 'a')
 
-#Parametros que me tocaron
-#10k personas = 20k genomas.
-#NePast = 20000 
-#Gen = 16N para tener burn-in 
-#10k * 16 = 160000 --> hagamos en cuello de botella en 80k
-#Dijimos que hariamos esquemas de tiempo y lo demas fijo a mi me toco reduccion a la mitad.
-#El tiempo es el que cambia y va en 3 esquemas... hagamos el primero que es 
-#El tiempo que dura el tamaño pasado.
+# Define default values
+DEFAULTS = {
+    'n_past_value': (20000),
+    'n_present_value': (10000),
+    'time_value': (80000),
+}
 
-#### Prior distributions
-NeInitial = 10000
-print(f"Ne Initial is :{NeInitial}")
-NePast = 20000
-print(f"Ne Past is :{NePast}")
-Time = 80000
-print(f"Time in generations is :{Time}\n")
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Passes fixed values for N_past, N_present, and Time.")
+parser.add_argument("--use_defaults", action="store_true", help="Use default parameter values")
+parser.add_argument("--n_past_value", type=int, required=False, help="N_past parameter value")
+parser.add_argument("--n_present_value", type=int, required=False, help="N_present parameter value")
+parser.add_argument("--time_value", type=int, required=False, help="Time in generations paramter value")
+parser.add_argument("--mutation_rate", type=float, required=False, help="mutation rate")
+parser.add_argument("--total_sites", type=int, required=False, help="Number of simulated sites")
+parser.add_argument("--step_size", type=int, required=False, help="for paralelization")
+
+# Parse arguments. change
+args = parser.parse_args()
+
+#pass these
+N_past = args.n_past_value
+N_present = args.n_present_value
+Time = args.time_value
+u = args.mutation_rate
+l = args.total_sites
+run_number = args.step_size
+
+#### Stable parameter values review 
+print(f"Ne in present time is :{N_present}")
+print(f"Ne in past is :{N_past}")
+print(f"Change took place at generation :{Time}\n")
+print(f"using mutation rate :{u}\n")
+print(f"Simulating a total of :{l} sites \n")
+print(f"divided into {run_number} independent simulation runs \n")
+
+#a ver entonces el archivo demografico es...
+#pasado Time yes. past and present. where time is duration of both sizes. 
+#presente Time. inference is done in first time.  
 
 ###Parametros de msel
-TimeFraction = float( int(Time) / ( 4.0 * NeInitial))
-PopFraction = float ( int (NePast) / ( 1.0 *  NeInitial))
+TimeFraction = float( int(Time) / ( 4.0 * N_present))
+PopFraction = float ( int (N_past) / ( 1.0 *  N_present))
 Rho=1 #recombination, but prefersim assumes no recombination so =1
 #Writes current drawn demography to file
-String = str(NeInitial) + "\t" + str(NePast) + "\t" + str (Time) + "\n"
+String = str(N_present) + "\t" + str(N_past) + "\t" + str (Time) + "\n"
 ParsFile.write(String)
 
 ###msel TimeFraction and PopFraction parameters for tree generation, this creates the file "LeftRandomNumbers"
@@ -62,12 +80,8 @@ RandFile.close()
 #Parameters subject to change depending on source data
 # l is the number of sites and dictates how much a single demography run is split. Paralel running.
 
-u = 2.35e-08 ##stdpopsim mut rate based on demography = 2.35e-8
-print(f"Using mutation rate {u}")
-l = 1000000 #Lets make this 1M even though in reality its 100M. What changes??? 
-print(f"Using total number of sites {l}")
-run_number=100
-theta = (NePast * 4 * u * l)
+l = l / run_number
+theta = (N_past * 4 * u * l)
 theta = str(theta)
 
 #Number identifica la corrida. #Cambia el nombre
@@ -127,9 +141,12 @@ FilePrefix: """ + OutDir + """
 ABCParsFileB.write(String)
 
 ##Archivo de demografia
-Ne = 2.0 * NeInitial
-String = str(int( 2.0 * NePast +0.5)) + " " + str(int((NePast * 16)+0.5)) + "\n"
-String = String + str(int(2.0 * NeInitial+0.5)) + " " + str(int((Time )+0.5)) + "\n"
+Ne = 2.0 * N_present
+String = str(int( 2.0 * N_past +0.5)) + " " + str(int((N_past * 16)+0.5)) + "\n"
+String = String + str(int(2.0 * N_present+0.5)) + " " + str(int((Time )+0.5)) + "\n"
+
+#So first Time value is strictly the burn in for first size. 
+#Second time value is what we are inferring, meaning, "generations ago that bottleneck happened". 
 
 #Tengo que agregar TimeFraction y PopFraction
 DemHistFile.write(String)
@@ -141,5 +158,5 @@ ABCParsFileB.close()
 RandFile.close()
 ParsFile.close()
 
-#This should be it... 
+#This should work. Now just change slurm. 
 
